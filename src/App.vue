5ca16b43009368046a1ae6e771be44395c8883a4
ReBeX <cswwwx@gmail.com>
2024-11-06 17:21:24 +0800

    feat: 调整界面、调整编辑操作、新增删除功能

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2024-10-28 17:43:37
  * @LastEditors: ReBeX  cswwwx@gmail.com
- * @LastEditTime: 2024-11-04 16:07:35
+ * @LastEditTime: 2024-11-06 17:16:03
  * @FilePath: \geojson-editor-ol\src\App.vue
  * @Description: 主页面
 -->
@@ -9,38 +9,121 @@
 <script setup>
 import { initMap } from '@/utils/mapView'
 import { VectorEditor } from '@/utils/vectorEditor'
+import GeoJSON from 'ol/format/GeoJSON.js'
 
 let map = null // 地图实例
 let drawVector = null // 绘制实例
-const drawOptions = ['Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon', 'Circle', 'Square', 'Box'] // 绘制类型
+const drawOptions = [ // 绘制类型
+  { type: 'Point', label: '点' },
+  { type: 'LineString', label: '线' },
+  { type: 'Polygon', label: '面' },
+  // { type: 'MultiPoint', label: '多点' },
+  // { type: 'MultiLineString', label: '多线' },
+  // { type: 'MultiPolygon', label: '多面' },
+  // { type: 'Circle', label: '圆' },
+  { type: 'Box', label: '矩形' },
+  { type: 'Square', label: '正方形' },
+]
+
+const currentActive = ref(null) // 当前激活的按钮
+const currentEditType = ref(null) // 当前编辑的类型
+const isEdit = ref(false) // 新增: 用于跟踪 isEdit 状态
+
+const featuresList = ref({ // 要素列表
+  type: 'FeatureCollection',
+  features: [],
+})
+
+const editButtons = [ // 操作按钮配置
+  { type: 'modify', label: '编辑折点' },
+  { type: 'translate', label: '平移' },
+  { type: 'delete', label: '删除' },
+]
+
+// 操作：绘制
+function handleDraw(type) {
+  isEdit.value = false
+
+  currentActive.value = type
+  currentEditType.value = null
+  drawVector.draw(type)
+}
+
+// 操作：进入编辑状态
+function enterEdit() {
+  currentActive.value = 'edit'
+  drawVector.edit()
+}
+
+// 操作：编辑状态下的其他操作
+function handleEdit(type) {
+  currentEditType.value = type
+  drawVector[type]()
+
+  if (type === 'delete') {
+    isEdit.value = false
+    currentEditType.value = null
+  }
+}
 
 onMounted(() => {
   map = initMap() // 初始化地图
   drawVector = new VectorEditor(map) // 初始化绘制实例
+
+  // 事件：监听要素变化，用于展示GeoJSON数据
+  const geojsonFormat = new GeoJSON()
+  drawVector._source.on('change', () => {
+    featuresList.value = geojsonFormat.writeFeaturesObject(drawVector._source.getFeatures())
+  })
+  // 事件：已选中要素时
+  drawVector._select.on('select', (event) => {
+    if (event.selected.length > 0) {
+      isEdit.value = true
+    }
+    else {
+      isEdit.value = false
+    }
+  })
 })
 </script>
 
 <template>
-  <div class="absolute top-0 left-0 z-10 flex items-start gap-2 flex-col">
-    <button v-for="item in drawOptions" :key="item" class="btn-ctl" @click="drawVector.draw(item)">
-      {{ item }}
+  <div class="absolute top-0 right-1/3 z-10 flex items-end gap-2 flex-col">
+    <button v-for="item in drawOptions" :key="item" :class="{ active: currentActive === item.type }" class="btn-ctl" @click="handleDraw(item.type)">
+      {{ item.label }}
     </button>
 
     <div class="w-full h-[1px] bg-gray-400" />
 
-    <button class="btn-ctl" @click="drawVector.modify">
-      编辑折点
+    <button :class="{ active: currentActive === 'edit' }" class="btn-ctl" @click="enterEdit">
+      编辑模式
     </button>
-    <button class="btn-ctl" @click="drawVector.translate">
-      拖拽
+
+    <button
+      v-for="btn in editButtons"
+      v-show="isEdit"
+      :key="btn.type"
+      class="btn-ctl"
+      :class="{ '!bg-red-300': currentEditType === btn.type }"
+      @click="handleEdit(btn.type)"
+    >
+      {{ btn.label }}
     </button>
   </div>
 
-  <div id="map" class="w-full h-full" />
+  <div class="w-full h-full flex">
+    <div id="map" class="w-2/3 h-full" />
+    <div class="w-1/3 h-full overflow-auto p-2 bg-gray-50">
+      <pre class="min-h-full bg-white p-2 rounded-lg shadow">{{ featuresList }}</pre>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .btn-ctl {
   @apply bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600 transition-colors;
+}
+.btn-ctl.active {
+  @apply bg-red-400;
 }
 </style>
--- src/utils/vectorEditor.js
+++ src/utils/vectorEditor.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-11-04 13:46:37
  * @LastEditors: ReBeX  cswwwx@gmail.com
- * @LastEditTime: 2024-11-04 20:50:04
+ * @LastEditTime: 2024-11-06 15:04:46
  * @FilePath: \geojson-editor-ol\src\utils\vectorEditor.js
  * @Description: 矢量编辑相关功能
  */
@@ -18,17 +18,19 @@ import { Vector as VectorSource } from 'ol/source.js'
  * @function: watch 监听
  * @function: modify 修改
  * @function: translate 移动
+ * @function: delete 删除
  * @function: stopDraw 停止绘制
  * @return {*}
  */
 export class VectorEditor {
   map = null // 地图实例
+
   _source = null // 数据源
   _vector = null // 图层
   _draw = null // 绘制交互
   _select = null // 选择交互
   _modify = null // 修改交互
-  _translate = null // 拖拽交互
+  _translate = null // 平移交互
   _snap = null // 吸附交互 | Notice: The snap interaction must be added last, as it needs to be the first to handle the pointermove event.
 
   constructor(map) {
@@ -58,6 +60,8 @@ export class VectorEditor {
     this.map.addInteraction(this._translate)
 
     this.stopDraw()
+
+    // 挂载事件
     this.watch.clearSelected()
   }
 
@@ -77,16 +81,48 @@ export class VectorEditor {
         console.log('绘制完成:', event.feature)
       })
     },
+    // 事件：已选中要素时
+    selected: () => {
+      this._select.on('select', (event) => {
+        console.log('选中要素:', event.selected)
+      })
+    },
   }
 
+  /** ------------------------------------------------------------ */
+
+  // 复位：停止绘制
+  stopDraw() {
+    if (this._draw) {
+      // this._draw.un('drawend', this.watch.drawEnd) // 先移除事件监听
+      this.map.removeInteraction(this._draw)
+      this._draw = null
+    }
+    this.map.removeInteraction(this._snap)
+    this._modify.setActive(false)
+    this._select.setActive(false)
+    this._translate.setActive(false)
+  }
+
+  // 复位：清除编辑状态下的互斥交互
+  stopMutual() {
+    // if (this._select.getFeatures().getArray().length === 0) {
+    //   console.warn('请先选中要素')
+    //   return
+    // }
+    this._modify.setActive(false)
+    this._translate.setActive(false)
+  }
+
+  /** ------------------------------------------------------------ */
+
   // 操作：绘制
+  // 所属互斥组：顶级功能组，重置方法 stopDraw
   // Options: 'Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon', 'Circle', 'Square', 'Box'
   draw(type) {
     this.stopDraw() // 停止绘制
-
     let value = type // 绘制类型
     let geometryFunction = null
-
     switch (type) {
       case 'Square':
         value = 'Circle'
@@ -97,48 +133,44 @@ export class VectorEditor {
         geometryFunction = createBox()
         break
     }
-
     this._draw = new Draw({
       source: this._source,
       type: value,
       geometryFunction, // 绘制回调
     })
-
-    // 添加绘制完成的监听器
-    this.watch.drawEnd()
-
     this.map.addInteraction(this._draw)
     this.map.addInteraction(this._snap)
   }
 
-  // 复位：停止绘制
-  stopDraw() {
-    if (this._draw) {
-      this._draw.un('drawend', this.watch.drawEnd) // 先移除事件监听
-      this.map.removeInteraction(this._draw)
-      this._draw = null
-    }
-    this.map.removeInteraction(this._snap)
-    this._modify.setActive(false)
-    this._select.setActive(false)
-    this._translate.setActive(false)
+  // 操作：进入编辑模式
+  // 所属互斥组：顶级功能组，重置方法 stopDraw
+  edit(flag = true) {
+    this.stopDraw()
+    this._select.setActive(flag)
   }
 
-  // 操作：修改
-  modify(flag = true) {
-    this.stopDraw()
+  /** ------------------------------------------------------------ */
 
-    this._select.setActive(flag)
+  // 操作：修改
+  // 所属互斥组：编辑模式组，重置方法 stopMutual
+  modify(flag = true) {
+    this.stopMutual()
     this._modify.setActive(flag)
     this.map.addInteraction(this._snap)
   }
 
-  // 操作：拖拽
+  // 操作：平移
+  // 所属互斥组：编辑模式组，重置方法 stopMutual
   translate(flag = true) {
-    this.stopDraw()
-
-    this._select.setActive(flag)
+    this.stopMutual()
     this._translate.setActive(flag)
     this.map.addInteraction(this._snap)
+  }
+
+  // 操作：删除
+  // 所属互斥组：编辑模式组，重置方法 stopMutual
+  delete() {
+    this.stopMutual()
+    this._source.removeFeature(this._select.getFeatures().getArray()[0])
   }
 }
