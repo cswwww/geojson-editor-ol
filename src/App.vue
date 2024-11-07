6322645afb3f9ce2e3a36e83021c5d4c67f25929
ReBeX <cswwwx@gmail.com>
2024-11-07 18:44:36 +0800

    feat: 新增挖孔功能

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2024-10-28 17:43:37
  * @LastEditors: ReBeX  cswwwx@gmail.com
- * @LastEditTime: 2024-11-06 17:16:03
+ * @LastEditTime: 2024-11-07 17:40:13
  * @FilePath: \geojson-editor-ol\src\App.vue
  * @Description: 主页面
 -->
@@ -13,56 +13,103 @@ import GeoJSON from 'ol/format/GeoJSON.js'
 
 let map = null // 地图实例
 let drawVector = null // 绘制实例
+const featuresList = ref({ // 右侧展示的要素列表
+  type: 'FeatureCollection',
+  features: [],
+})
 const drawOptions = [ // 绘制类型
   { type: 'Point', label: '点' },
   { type: 'LineString', label: '线' },
   { type: 'Polygon', label: '面' },
-  // { type: 'MultiPoint', label: '多点' },
-  // { type: 'MultiLineString', label: '多线' },
-  // { type: 'MultiPolygon', label: '多面' },
-  // { type: 'Circle', label: '圆' },
-  { type: 'Box', label: '矩形' },
+  { type: 'Box', label: '矩形(Q)' },
   { type: 'Square', label: '正方形' },
 ]
 
 const currentActive = ref(null) // 当前激活的按钮
 const currentEditType = ref(null) // 当前编辑的类型
-const isEdit = ref(false) // 新增: 用于跟踪 isEdit 状态
-
-const featuresList = ref({ // 要素列表
-  type: 'FeatureCollection',
-  features: [],
-})
+const isEdit = ref(false) // 是否处于编辑状态
 
 const editButtons = [ // 操作按钮配置
-  { type: 'modify', label: '编辑折点' },
-  { type: 'translate', label: '平移' },
+  { type: 'modify', label: '编辑折点(E)' },
+  { type: 'translate', label: '平移(R)' },
   { type: 'delete', label: '删除' },
+  { type: 'cutHole', label: '挖孔(A)' },
 ]
 
 // 操作：绘制
 function handleDraw(type) {
+  if (currentActive.value === type) {
+    currentActive.value = null
+    drawVector.stopDraw()
+    return
+  }
   isEdit.value = false
-
   currentActive.value = type
   currentEditType.value = null
   drawVector.draw(type)
 }
 
-// 操作：进入编辑状态
+// 操作：编辑
 function enterEdit() {
+  if (currentActive.value === 'edit') { // 如果当前处于编辑状态，则退出编辑状态
+    currentActive.value = null
+    isEdit.value = false
+    currentEditType.value = null
+    drawVector.stopDraw()
+    return
+  }
   currentActive.value = 'edit'
   drawVector.edit()
 }
 
 // 操作：编辑状态下的其他操作
 function handleEdit(type) {
-  currentEditType.value = type
-  drawVector[type]()
+  switch (type) {
+    case 'modify':
+      drawVector[type](type !== currentEditType.value)
+      currentEditType.value = type === currentEditType.value ? null : type
+      break
+    case 'translate':
+      drawVector[type](type !== currentEditType.value)
+      currentEditType.value = type === currentEditType.value ? null : type
+      break
+    case 'cutHole':
+      drawVector.cutHole(type !== currentEditType.value)
+      currentEditType.value = type === currentEditType.value ? null : type
+      break
+    case 'delete':
+      drawVector[type]()
+      isEdit.value = false
+      currentEditType.value = null
+      break
+  }
+}
 
-  if (type === 'delete') {
-    isEdit.value = false
-    currentEditType.value = null
+// 事件：键盘事件
+function keyDown(e) {
+  const key = e.key.toLowerCase()
+  switch (key) {
+    case 'q':
+      handleDraw('Box')
+      break
+    case 'w':
+      enterEdit()
+      break
+    case 'e':
+      if (isEdit.value) {
+        handleEdit('modify')
+      }
+      break
+    case 'r':
+      if (isEdit.value) {
+        handleEdit('translate')
+      }
+      break
+    case 'a':
+      if (isEdit.value) {
+        handleEdit('cutHole')
+      }
+      break
   }
 }
 
@@ -84,11 +131,17 @@ onMounted(() => {
       isEdit.value = false
     }
   })
+
+  window.addEventListener('keydown', keyDown) // 事件：监听键盘事件
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', keyDown, false)
 })
 </script>
 
 <template>
-  <div class="absolute top-0 right-1/3 z-10 flex items-end gap-2 flex-col">
+  <div class="select-none absolute top-0 right-1/3 z-10 flex items-end gap-2 flex-col">
     <button v-for="item in drawOptions" :key="item" :class="{ active: currentActive === item.type }" class="btn-ctl" @click="handleDraw(item.type)">
       {{ item.label }}
     </button>
@@ -96,7 +149,7 @@ onMounted(() => {
     <div class="w-full h-[1px] bg-gray-400" />
 
     <button :class="{ active: currentActive === 'edit' }" class="btn-ctl" @click="enterEdit">
-      编辑模式
+      编辑模式(W)
     </button>
 
     <button
--- src/utils/vectorEditor.js
+++ src/utils/vectorEditor.js
@@ -1,11 +1,13 @@
 /*
  * @Date: 2024-11-04 13:46:37
  * @LastEditors: ReBeX  cswwwx@gmail.com
- * @LastEditTime: 2024-11-06 15:04:46
+ * @LastEditTime: 2024-11-07 18:43:22
  * @FilePath: \geojson-editor-ol\src\utils\vectorEditor.js
  * @Description: 矢量编辑相关功能
  */
 
+import * as turf from '@turf/turf'
+import GeoJSON from 'ol/format/GeoJSON.js'
 import { Draw, Modify, Select, Snap, Translate } from 'ol/interaction.js'
 import { createBox, createRegularPolygon } from 'ol/interaction/Draw.js'
 import { Vector as VectorLayer } from 'ol/layer.js'
@@ -19,19 +21,26 @@ import { Vector as VectorSource } from 'ol/source.js'
  * @function: modify 修改
  * @function: translate 移动
  * @function: delete 删除
+ * @function: cutHole 挖孔
  * @function: stopDraw 停止绘制
  * @return {*}
  */
+
 export class VectorEditor {
   map = null // 地图实例
 
-  _source = null // 数据源
-  _vector = null // 图层
+  _source = null // 基础数据源
+  _vector = null // 基础图层
+  _editSource = null // 编辑用数据源
+  _editVector = null // 编辑用图层
+
   _draw = null // 绘制交互
   _select = null // 选择交互
   _modify = null // 修改交互
   _translate = null // 平移交互
   _snap = null // 吸附交互 | Notice: The snap interaction must be added last, as it needs to be the first to handle the pointermove event.
+  _drawHole = null // 挖孔交互
+  _selectedFeature = null // 编辑状态下选中的要素
 
   constructor(map) {
     this.map = map // 引入地图实例
@@ -41,19 +50,16 @@ export class VectorEditor {
   // 操作：初始化
   init() {
     this._source = new VectorSource()
-    this._vector = new VectorLayer({
-      source: this._source,
-    })
+    this._editSource = new VectorSource()
+    this._vector = new VectorLayer({ source: this._source })
+    this._editVector = new VectorLayer({ source: this._editSource })
     this.map.addLayer(this._vector)
+    this.map.addLayer(this._editVector)
 
-    this._select = new Select() // 实例化选择交互
-    this._modify = new Modify({ // 实例化修改交互
-      features: this._select.getFeatures(),
-    })
-    this._translate = new Translate({
-      features: this._select.getFeatures(),
-    })
     this._snap = new Snap({ source: this._source }) // 实例化吸附交互
+    this._select = new Select() // 实例化选择交互
+    this._modify = new Modify({ features: this._select.getFeatures() }) // 实例化修改交互
+    this._translate = new Translate({ features: this._select.getFeatures() }) // 实例化平移交互
 
     this.map.addInteraction(this._modify)
     this.map.addInteraction(this._select)
@@ -61,8 +67,7 @@ export class VectorEditor {
 
     this.stopDraw()
 
-    // 挂载事件
-    this.watch.clearSelected()
+    this.watch.clearSelected() // 挂载事件
   }
 
   // 监听：各类事件
@@ -85,6 +90,16 @@ export class VectorEditor {
     selected: () => {
       this._select.on('select', (event) => {
         console.log('选中要素:', event.selected)
+      })
+    },
+    // 事件：挖孔交互结束时
+    drawHoleEnd: () => {
+      this._drawHole.on('drawend', (event) => {
+        const mask = turf.polygon(this._editSource.getFeatures()[0].getGeometry().getCoordinates()) // 被挖区域
+        const polygon = turf.polygon(event.feature.getGeometry().getCoordinates()) // 绘制区域
+        const result = new GeoJSON().readFeatures(turf.mask(polygon, mask))
+        this._editSource.clear()
+        this._editSource.addFeatures(result)
       })
     },
   }
@@ -112,6 +127,15 @@ export class VectorEditor {
     // }
     this._modify.setActive(false)
     this._translate.setActive(false)
+
+    if (this._drawHole) {
+      this._drawHole.un('drawend', this.watch.drawHoleEnd) // 先移除事件监听
+      this.map.removeInteraction(this._drawHole)
+      this._drawHole = null
+      this._select.setActive(true)
+      this._vector.setVisible(true) // 显示基础图层
+      this._editSource.clear()
+    }
   }
 
   /** ------------------------------------------------------------ */
@@ -144,9 +168,9 @@ export class VectorEditor {
 
   // 操作：进入编辑模式
   // 所属互斥组：顶级功能组，重置方法 stopDraw
-  edit(flag = true) {
+  edit() {
     this.stopDraw()
-    this._select.setActive(flag)
+    this._select.setActive(true)
   }
 
   /** ------------------------------------------------------------ */
@@ -155,16 +179,22 @@ export class VectorEditor {
   // 所属互斥组：编辑模式组，重置方法 stopMutual
   modify(flag = true) {
     this.stopMutual()
-    this._modify.setActive(flag)
-    this.map.addInteraction(this._snap)
+
+    if (flag) {
+      this._modify.setActive(true)
+      this.map.addInteraction(this._snap)
+    }
   }
 
   // 操作：平移
   // 所属互斥组：编辑模式组，重置方法 stopMutual
   translate(flag = true) {
     this.stopMutual()
-    this._translate.setActive(flag)
-    this.map.addInteraction(this._snap)
+
+    if (flag) {
+      this._translate.setActive(true)
+      this.map.addInteraction(this._snap)
+    }
   }
 
   // 操作：删除
@@ -172,5 +202,33 @@ export class VectorEditor {
   delete() {
     this.stopMutual()
     this._source.removeFeature(this._select.getFeatures().getArray()[0])
+  }
+
+  // 操作：挖孔
+  // 所属互斥组：编辑模式组，重置方法 stopMutual
+  cutHole(flag = true) {
+    if (flag && this._select.getFeatures().getArray().length > 0) { // 开挖
+      this.stopMutual() // 停止互斥交互
+      this._vector.setVisible(false) // 隐藏基础图层
+      this._selectedFeature = this._select.getFeatures().getArray()[0] // 记录选中的要素
+      this._selectedFeature && this._editSource.addFeature(this._selectedFeature)
+      this._select.setActive(false)
+      this._drawHole = new Draw({
+        source: new VectorSource(),
+        type: 'Polygon',
+      })
+      this.watch.drawHoleEnd() // 添加事件监听
+      this.map.addInteraction(this._drawHole)
+    }
+    else if (this._drawHole) { // 停止
+      this._source.removeFeature(this._selectedFeature) // 移除被编辑的要素
+      this._source.addFeature(this._editSource.getFeatures()[0]) // 添加编辑后的要素
+      this._vector.setVisible(true) // 显示基础图层
+      this._editSource.clear()
+      this._drawHole.un('drawend', this.watch.drawHoleEnd) // 先移除事件监听
+      this.map.removeInteraction(this._drawHole)
+      this._drawHole = null
+      this._select.setActive(true)
+    }
   }
 }
